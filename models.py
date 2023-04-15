f8df5d09a5f99b5c56d88ec1274685fa885b2dd5
михаил зайцев <insdxout@gmail.com>
2023-04-15 19:47:31 +0600

    updated models

--- models.py
+++ models.py
@@ -151,7 +151,7 @@ class RVEAttention_MH(nn.Module):
     RVE Model with MultiHead attention.
     """
 
-    def __init__(self,  encoder, attention_embed_dim, attention_num_heads, attention_dropout, decoder=None,
+    def __init__(self,  encoder, attention_embed_dim, batchnorm_dim, attention_num_heads, attention_dropout, decoder=None,
                  reconstruct=False, dropout_regressor=0, regression_dims=200):
         super(RVEAttention_MH, self).__init__()
         self.decode_mode = reconstruct
@@ -163,7 +163,7 @@ class RVEAttention_MH(nn.Module):
         self.regression_dims = regression_dims
         self.self_attention = nn.MultiheadAttention(embed_dim=attention_embed_dim, num_heads=attention_num_heads,
                                                     dropout=attention_dropout, batch_first=True)
-        self.batchnorm = nn.BatchNorm1d(attention_embed_dim)
+        self.batchnorm = nn.BatchNorm1d(batchnorm_dim)
   
         self.regressor = nn.Sequential(
             nn.Linear(self.encoder.latent_dim, self.regression_dims),
@@ -186,8 +186,9 @@ class RVEAttention_MH(nn.Module):
         """
         x = torch.permute(x, (0, 2, 1))
         x, _ = self.self_attention(x, x, x)
-        x = torch.permute(x, (0, 2, 1))
         x = self.batchnorm(x)
+        x = torch.permute(x, (0, 2, 1))
+        
         z, mean, log_var = self.encoder(x)
         y_hat = self.regressor(z)
         if self.decode_mode:
@@ -221,7 +222,7 @@ class RVEAttention_MP(nn.Module):
     """
     RVE Model with Multiplicative attention.
     """
-    def __init__(self,  encoder, attention_values_embedding, attention_queries_embedding, decoder=None,
+    def __init__(self,  encoder, attention_values_embedding, batchnorm_dim, attention_queries_embedding, decoder=None,
                  reconstruct=False, dropout_regressor=0, regression_dims=200):
         super(RVEAttention_MP, self).__init__()
         self.decode_mode = reconstruct
@@ -232,7 +233,7 @@ class RVEAttention_MP(nn.Module):
         self.p = dropout_regressor
         self.regression_dims = regression_dims
         self.self_attention = MultiplicativeAttention(values_embedding=attention_values_embedding, queries_embedding=attention_queries_embedding)
-        self.batchnorm = nn.BatchNorm1d(attention_values_embedding, affine=False)
+        self.batchnorm = nn.BatchNorm1d(batchnorm_dim, affine=False)
   
         self.regressor = nn.Sequential(
             nn.Linear(self.encoder.latent_dim, self.regression_dims),
@@ -255,8 +256,8 @@ class RVEAttention_MP(nn.Module):
         """
         x = torch.permute(x, (0, 2, 1))
         x = self.self_attention(x, x)
-        x = torch.permute(x, (0, 2, 1))
         x = self.batchnorm(x)
+        x = torch.permute(x, (0, 2, 1))
         z, mean, log_var = self.encoder(x)
         y_hat = self.regressor(z)
         if self.decode_mode:
